5b5147678bdfaa5aaac67dc9ca9aef35adc91f87
Ieuan Higgs <ieuanhiggs@gmail.com>
2025-12-03 10:31:44 +0000

    Overhaul

---
+++ docs/assets/js/scripts.js
@@ -0,0 +1,4 @@
+// Example JavaScript to enhance functionality. Can be expanded.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to the personal page of Dr. Ieuan Higgs!");
+});
